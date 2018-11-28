4277767b7d6514446caf0ae73b969420df1e1dae
Teemo111 <214761795@qq.com>
2018-11-28 17:32:04 +0800

    citypicker go on

--- app/src/main/java/com/mili/activity/CityPickerActivity.java
+++ app/src/main/java/com/mili/activity/CityPickerActivity.java
@@ -1,24 +1,15 @@
 package com.mili.activity;
 
-import com.mili.base.BaseActivity;
+import com.mili.R;
+import com.mili.base.Label2UIActivity;
 
 /**
  * Created by TeeMo111 on 2018/11/27.
  */
-public class CityPickerActivity extends BaseActivity {
-    @Override
-    protected void fetchExtra() {
-
-    }
-
-    @Override
-    protected void initToolbar() {
-
-    }
-
+public class CityPickerActivity extends Label2UIActivity {
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_city_picker;
     }
 
     @Override
