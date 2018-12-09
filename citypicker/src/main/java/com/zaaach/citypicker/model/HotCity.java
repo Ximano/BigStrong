e32889e2bb8440b64de54c848f6f386f1371f713
Ximano <Ximano521>
2018-12-09 14:54:17 +0800

    delete citypicker for a short time

--- citypicker/src/main/java/com/zaaach/citypicker/model/HotCity.java
+++
@@ -1,8 +0,0 @@
-package com.zaaach.citypicker.model;
-
-public class HotCity extends City {
-
-    public HotCity(String name, String id) {
-        super(name, "热门省份", id);
-    }
-}
--- citypicker/src/main/java/com/zaaach/citypicker/model/LocatedCity.java
+++
@@ -1,8 +0,0 @@
-package com.zaaach.citypicker.model;
-
-public class LocatedCity extends City {
-
-    public LocatedCity(String name, String id) {
-        super(name, "全国省份", id);
-    }
-}
